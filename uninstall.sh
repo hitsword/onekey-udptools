a43279e2bc2bb1a5bc0876dba53ca99433d1a7e8
HITSword <admin@huayizhiyun.com>
2020-03-05 16:34:51 +0800

    卸载优化判断
    Signed-off-by: HITSword <admin@huayizhiyun.com>

--- uninstall.sh
+++ uninstall.sh
@@ -3,17 +3,17 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 echo "杀掉进程"
-if ps aux | grep -q -e "udp2raw"; then
+if ps aux | grep -q -e "udp2raw" | grep -v grep; then
   killall udp2raw
 fi
-if ps aux | grep -q -e "udpspeeder"; then
+if ps aux | grep -q -e "udpspeeder" | grep -v grep; then
   killall udpspeeder
 fi
 echo "删除所有udptools文件"
 rm -rf /usr/local/udptools
 echo "删除相关服务"
 #判断服务模式
-if pgrep systemd-journal; then
+if pgrep systemd-journal > /dev/null; then
     if ls /usr/lib/systemd/system/ | grep -q -e "udp2raw*.service"; then
       find /usr/lib/systemd/system/ -name "udp2raw*.service" | xargs -0 rm
     fi
