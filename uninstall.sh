3f1ce51dc49d82046288d9420bc24db1ca97e00d
HITSword <admin@huayizhiyun.com>
2020-03-05 16:30:05 +0800

    卸载增加判断
    Signed-off-by: HITSword <admin@huayizhiyun.com>

--- uninstall.sh
+++ uninstall.sh
@@ -10,9 +10,17 @@ rm -rf /usr/local/udptools
 echo "删除相关服务"
 #判断服务模式
 if pgrep systemd-journal; then
-    find /usr/lib/systemd/system/ -name "udp2raw*.service" | xargs -0 rm
-    find /usr/lib/systemd/system/ -name "udpspeeder*.service" | xargs -0 rm
+    if ls /usr/lib/systemd/system/ | grep -q -e "udp2raw*.service"; then
+      find /usr/lib/systemd/system/ -name "udp2raw*.service" | xargs -0 rm
+    fi
+    if ls /usr/lib/systemd/system/ | grep -q -e "udpspeeder*.service"; then
+      find /usr/lib/systemd/system/ -name "udpspeeder*.service" | xargs -0 rm
+    fi
   else
-    find /etc/init.d/ -name "udp2raw*" | xargs -0 rm
-    find /etc/init.d/ -name "udpspeeder*" | xargs -0 rm
+    if ls /etc/init.d/ | grep -q -e "udp2raw*"; then
+      find /etc/init.d/ -name "udp2raw*" | xargs -0 rm
+    fi
+    if ls /etc/init.d/ | grep -q -e "udpspeeder*"; then
+      find /etc/init.d/ -name "udpspeeder*" | xargs -0 rm
+    fi
 fi
